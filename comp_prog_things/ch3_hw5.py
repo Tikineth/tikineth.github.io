5d41d82eea6bfa59585e74d2042b7dc06a7da28e
Tikineth <billonesv696@gmail.com>
2026-09-16 08:36:18 -0500

    Delete comp_prog_things directory

--- comp_prog_things/ch3_hw5.py
+++
@@ -1,7 +0,0 @@
-kg = int(input("what is the object mass in kilograms?: "))
-weight =9.8*kg
-
-if (weight > 500):
-    print("The object is too heavy.")
-elif (weight < 100):
-    print("The object is too light.")
