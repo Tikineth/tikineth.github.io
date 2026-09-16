5b289d7161a91c18ed0c5f3dbdb680de1d690d66
Tikineth <billonesv696@gmail.com>
2026-09-16 09:17:47 -0500

    Add files via upload

---
+++ comp_prog_things/Miscellaneous.py
@@ -0,0 +1,21 @@
+print("Hello, World!")
+
+for i in range(5):
+    print("potato")
+
+
+name = "hapless fool"
+nameber = 790
+
+print("That " + name + " tried " + str(nameber) + " before he was able to kill me.")
+
+
+ask_num = int(input("What is your favorite number? "))
+
+ask_num1 = int(input("What is your second favorite number? "))
+
+print("The sum of your favorite numbers is: " + str(ask_num + ask_num1))
+
+def global_loop():
+    for i in range(5):
+        print("This is a loop.")
